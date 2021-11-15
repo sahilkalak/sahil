d7d83773bc55f82821597f55062ce4144451e245
sahilkalak <94354842+sahilkalak@users.noreply.github.com>
2021-11-15 14:08:32 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,24 @@
+import numpy
+
+from scipy import stats
+
+
+speed 
+speed= [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+
+x = numpy.mean(speed)
+
+
+y= numpy.median(speed)
+
+
+z = stats.mode(speed)
+
+
+print(x)
+print(y)
+
+
+print(z)
+ 
